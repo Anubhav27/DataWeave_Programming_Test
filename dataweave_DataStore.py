a061899711ae3eb734b9b88ad8ddcb37a6b39622
Anubhav27 <anubhav.nanda@gmail.com>
2016-09-09 11:26:46 +0530

    assignment

--- dataweave_DataStore.py
+++ dataweave_DataStore.py
@@ -3,30 +3,54 @@ class DataStore:
     l = []
     def __init__(self,num_versions):
         self.num_versions = num_versions
-        self.l.append(1);
+
 
     def add(self,key,value):
         print "adding key value in a datastore"
-        print len(l)
+        local_list = []
+        if key in self.dataStore.keys():
+            print "key with %s is there so appending to it" % key
+            local_list = self.dataStore.get(key)
+            if (len(local_list) < int(self.num_versions)):
+                local_list.append(value)
+                self.dataStore[key] = local_list
+            print self.dataStore
+        else:
+            print "key %s is not there" % key
+            local_list.append(value)
+            self.dataStore[key] = local_list
+            print self.dataStore
+
 
     def get(self,key):
-        print "return latest value of key"
-
-    def get(self,key,latest_value):
-        print "return latest value of key"
+        print "return latest value of %s" % key
+        loc_list = self.dataStore[key]
+        print loc_list[len(loc_list)-1]
 
     def delete(self,key):
-        print "delete a key"
+        del self.dataStore[key]
+        print "key %s is deleted" % key
+
+
 
 
 
 version=""
 with open('datastore_configuration') as f:
     for line in f:
-        print line
         version = line
 
 ds = DataStore(version)
 ds.add(1,1)
 ds.add(1,2)
 ds.add(1,3)
+
+ds.add(2,1)
+ds.add(2,2)
+ds.add(2,3)
+
+ds.get(1)
+
+ds.delete(1)
+
+
